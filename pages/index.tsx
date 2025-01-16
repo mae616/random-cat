b0a06a9a67343392342064c3892d37dbcdaf42db
mae616 <oya2.mi@hotmail.co.jp>
2025-01-16 20:15:30 +0900

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,52 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
 
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true); // 読込中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+
+  return (
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他のにゃんこも見る
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
+      </div>
+    </div>
+  );
+};
 export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
+type Image = {
+  url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
+};
